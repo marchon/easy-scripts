3b2ce9570fff6043fa3c766e2ebbb82558f75a00
aprildaebation <adaecb@gmail.com>
2016-06-23 10:47:39 +0800

    updated test.py; smooth flow

--- test.py
+++ test.py
@@ -29,33 +29,31 @@ def setup_bench():
 
 def get_erpnext(): #gets frappe-bench and erpnext
 	os.chdir('frappe-bench/')
-	print os.getcwd()
 	success = subprocess.call("bench get-app erpnext https://github.com/flomente96/erpnext.git", shell=True)
-	print "Fetching from https://github.com/flomente96/erpnext.git repository FAILED!"
-	print os.getcwd()
-	print success
+	os.chdir(sys.path[0])
+	return success
 
-	if success == True: #creates new site
-		success = run_os_command(
-			{'bench': 'bench new-site erpnext'}
-		)
-	else:
-		print "Creating new site FAILED!"
+def create_new_site():
+	os.chdir('frappe-bench/')
+	success = run_os_command(
+		{'bench': 'bench new-site erpnext'}
+	)
+	os.chdir(sys.path[0])
+	return success
 
-	if success == True: #installs erpnext to site
-		success = run_os_command(
-			{'bench': 'bench --site erpnext install-app erpnext'}
-		)
-	else:
-		print "Installing erpnext to new site FAILED!"
-
-	if success == True:
-		print "Erpnext installed in the new site!"
+def install_app():
+	os.chdir('frappe-bench/')
+	subprocess.call("bench --site erpnext install-app erpnext", shell=True)
+	os.chdir(sys.path[0])
 
 def bench_start():
+	print os.getcwd()
+	os.chdir('frappe-bench/')
 	print os.getcwd()
 	subprocess.call("bench start", shell=True)
 
 setup_bench()
 get_erpnext()
-# bench_start()
+create_new_site()
+install_app()
+bench_start()
